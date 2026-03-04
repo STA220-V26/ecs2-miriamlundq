3cce96ea7c3426a65a569f1fb3f26b8924e53b87
Miriam Lundqvist <m.lundqvist@outlook.com>
2026-03-04 21:50:39 +0100

    #3.5 second

--- _targets.R
+++ _targets.R
@@ -53,8 +53,8 @@ list(
 
   # TODO: Something related to zip should be added here:
   # Unzip the downloaded dataset and return the extracted file names
-  tar_target(csv_files, zip::unzip(zipdata))
-)
+  tar_target(csv_files, zip::unzip(zipdata)),
+
 
 
 
@@ -68,7 +68,7 @@ list(
     tar_target(dt, fread(path)),
    names = name,
    descriptions = NULL
-  )
+  ),
 
 
 #extra things we did in between
@@ -83,24 +83,10 @@ list(
   # TODO: something related to codebook should be added here
 ## Run the get_codebook() function and save the result as a target called codebook
 
-tar_target(codebook, get_codebook())
+tar_target(codebook, get_codebook()),
 #after this we did tar_make() again to add the codebook as a target in the pipeline?
 
-## Copied from instruction: 
-tar_load(codebook)
-cb_patients <- codebook[file == "patients.csv"]
-# Which columns are relevant for `patients`?
-cb_patients[, column_name]
-# Well, those columns don't actually match the column names used in the data!
-# Do they?
-names(dt_patients)
-# Do you notice a pattern in how the names differ?  #Answer: they differ in big or small letters
-# Let's fix that
-cb_patients[, column_name := tolower(column_name)]
-# Do we have any variables which are not documented?
-setdiff(names(dt_patients), cb_patients[, column_name]) # Non-documented
-# Yep, so let's fix that manually for now
-cb_patients[, column_name := gsub(" county code", "", column_name)]
+
 
 #Are there any additional columns from other datasets which are not described correctly by the
 #codebook? Answer: no becuase setdiff returns 0?
@@ -109,3 +95,5 @@ cb_patients[, column_name := gsub(" county code", "", column_name)]
   # TODO: Something related to data_scans should be added here
 
 tar_target(data_scan_patient, export_data_scan(dt_patients, "patients"), format = "file")
+)
+
